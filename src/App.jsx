457c2eeabf62c5fbf38bcc9bb54f6154c123d275
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-16 15:57:25 +0800

    refactor: added a inbox and notification route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Inbox from './pages/Inbox';
+import Notification from './pages/Notification';
 
 export default class App extends Component {
   render() {
@@ -12,6 +14,8 @@ export default class App extends Component {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/inbox" element={<Inbox />} />
+          <Route path="/notifications" element={<Notification />} />
         </Routes>
       </Router>
     );
