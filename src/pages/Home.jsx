1795debb7229d5a1075b53d4a7299c3f009b2913
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-16 12:01:48 +0800

    refactor: added a home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useUser } from "../hooks/useUser";
+import TopMenu from "../components/topMenu";
+
+function Home() {
+    const user = useUser();
+
+    if (!user) return <p>Loading...</p>;
+
+    return (
+        <div className="bg-[#FCF1E3] min-h-screen">
+            <TopMenu />
+            <div className="flex flex-col md:flex-row gap-4 md:gap-10 mt-10 px-4 md:px-30">
+                <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Tools</p>
+                <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Car</p>
+                <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Clothing & Accessories</p>
+                <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Electronics</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
