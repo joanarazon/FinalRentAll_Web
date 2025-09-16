1549e99ec739338b409f3965b280846e0ab440cf
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-16 12:12:06 +0800

    refactor: added an items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,9 @@ function Home() {
                 <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Electronics</p>
                 <p className="cursor-pointer px-2 py-1 md:px-0 md:py-0">Others</p>
             </div>
+            <div className="flex flex-col md:flex-row gap-4 md:gap-10 mt-10 px-4 md:px-30">
+                <h1 className="text-3xl font-semibold px-2 py-1 md:px-0 md:py-0">Items</h1>
+            </div>
         </div>
     );
 }
